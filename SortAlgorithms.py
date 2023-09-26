92ea4231c1d844c661b6fc854ae39b052cc2b873
dom <dmacisaac@ewu.edu>
2023-09-26 15:35:54 -0700

    change

--- SortAlgorithms.py
+++ SortAlgorithms.py
@@ -5,7 +5,7 @@ def CountSort(A,B,k):
     for i in range(n-1):
         C[A[i]] = C[A[i]] + 1
 
-    for i in range(1,k):
+    for i in range(1,k-1):
         C[i] = C[i] + C[i-1]
     
     for i in range(n-1,0,-1):
@@ -13,9 +13,10 @@ def CountSort(A,B,k):
         C[A[i]] = C[A[i]] - 1
     return B
 
-A = [1,5,3,6,9,3,5,7,2,6,8,6,6,1,4]
-B = [0] * (len(A))
-k = 10
+if __name__=="__main__":
+    A = [1,5,3,6,9,3,5,7,2,6,8,6,6,1,4]
+    B = [0] * (len(A))
+    k = 10
 
-CountSort(A,B,k)
-print(B)
+    CountSort(A,B,k)
+    print(B)
