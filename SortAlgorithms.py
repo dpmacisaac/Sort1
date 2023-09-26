2761de3b85ba1a85c15c3627980b28825c054f23
dom <dmacisaac@ewu.edu>
2023-09-26 15:33:44 -0700

    work

--- SortAlgorithms.py
+++ SortAlgorithms.py
@@ -5,8 +5,7 @@ def CountSort(A,B,k):
     for i in range(n-1):
         C[A[i]] = C[A[i]] + 1
 
-    for i in range(1,n-1):
-        print(i)
+    for i in range(1,k):
         C[i] = C[i] + C[i-1]
     
     for i in range(n-1,0,-1):
